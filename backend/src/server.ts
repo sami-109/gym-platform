2f301bce4f16444ca4725bf0eceb2901f99e0782
sami <samirachid109@gmail.com>
2026-09-11 18:02:33 +0300

    Add login frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,18 @@ import gymRoutes from "./routes/gym.routes.js";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/gyms", gymRoutes);
